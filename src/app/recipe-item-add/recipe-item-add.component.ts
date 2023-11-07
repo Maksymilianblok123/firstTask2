53c99e20bd12f6c2ed6b9c4d71da3c3d031a91e3
Maksymilian Blok <maksymilian.blok@houseofangular.io>
2023-11-07 09:35:29 +0100

    wip

---
+++ src/app/recipe-item-add/recipe-item-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-item-add',
+  templateUrl: './recipe-item-add.component.html',
+  styleUrls: ['./recipe-item-add.component.scss']
+})
+export class RecipeItemAddComponent {
+
+}
---
+++ src/app/recipe-list-item/recipe-list-item.component.ts
@@ -0,0 +1,54 @@
+import {Component, Input} from '@angular/core';
+import {Recipe} from "../shared/interfaces/recipe/recipe";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {ConfirmationModalComponent} from "../shared/components/confirmation-modal/confirmation-modal.component";
+import {Router} from "@angular/router";
+import {RecipesService} from "../services/recipes/recipes.service";
+import {FormBuilder} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatDialog} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-recipe-list-item',
+  templateUrl: './recipe-list-item.component.html',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatIconModule
+  ],
+  styleUrls: ['./recipe-list-item.component.scss']
+})
+export class RecipeListItemComponent {
+  @Input() recipe!: Recipe;
+
+  constructor(
+    private router: Router,
+    private recipeService: RecipesService,
+    private _snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) {}
+
+
+  removeRecipe(recipe: Recipe) {
+    const dialogRef = this.dialog.open(ConfirmationModalComponent, {
+      data: { text: `Czy napewno chcesz usunąć przepis '${recipe.name}'` },
+    });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        this.recipeService.deleteRecipe(recipe._id)
+          .subscribe(() => {
+            this._snackBar.open(`Usunięto element ${recipe.name}`, `OK`)
+          }, () => {
+            this._snackBar.open(`Wystąpił błąd podczas usuwania`, `OK`)
+          })
+      }
+    });
+  }
+
+
+  navigateToEdit(_id: string) {
+    this.router.navigate(['recipe/' + _id +'/edit'])
+  }
+}
